7477b79f6f2c9730a2b2eb5a3faedf4dc1f6194c
kikro1 <bogdash.voitencov.2008@gmail.com>
2026-09-09 13:56:25 +0100

    Address review: exit process on DB failure, use returnDocument in update
    - connectMongoDB: process.exit(1) in catch instead of rethrowing
    - updateNote: { returnDocument: 'after' } (Mongoose v9, replaces { new: true })

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -25,7 +25,9 @@ export const createNote = async (req, res) => {
 
 export const updateNote = async (req, res) => {
   const { noteId } = req.params;
-  const note = await Note.findByIdAndUpdate(noteId, req.body, { new: true });
+  const note = await Note.findByIdAndUpdate(noteId, req.body, {
+    returnDocument: 'after',
+  });
 
   if (!note) {
     throw createHttpError(404, 'Note not found');
