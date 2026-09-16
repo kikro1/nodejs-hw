fde3fd9c0620ae71a968e5c23b3a218710f67a5f
kikro1 <bogdash.voitencov.2008@gmail.com>
2026-09-16 13:40:55 +0100

    Map multer errors (e.g. oversized file) to 400 instead of falling through to 500

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -1,8 +1,15 @@
 import { isHttpError } from 'http-errors';
+import { MulterError } from 'multer';
 
 export const errorHandler = (err, req, res, next) => {
   if (isHttpError(err)) {
     return res.status(err.status).json({
+      message: err.message,
+    });
+  }
+
+  if (err instanceof MulterError) {
+    return res.status(400).json({
       message: err.message,
     });
   }
